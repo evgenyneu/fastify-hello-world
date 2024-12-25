f7a6ec2ff29425bad25ec5a5fa4f9c1b37d998ca
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2024-12-26 07:06:06 +1100

    Work

--- routes/root.js
+++ routes/root.js
@@ -2,6 +2,7 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
+    reply.header('x-filler', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
     return 'Hello, World!'
   })
 }
