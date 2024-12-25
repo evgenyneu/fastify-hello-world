3106282ab39c7621a827c788f3fd51d5eff02b12
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2024-12-26 07:50:49 +1100

    work

--- cluster.js
+++ cluster.js
@@ -1,5 +1,7 @@
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
+const path = require('path');
+const Fastify = require('fastify');
 
 if (cluster.isPrimary) {
   console.log(`Primary ${process.pid} is running`);
@@ -13,5 +15,21 @@ if (cluster.isPrimary) {
     console.log(`worker ${worker.process.pid} died`);
   });
 } else {
-  // Run the app
+  // Run the Fastify app
+  process.env.NODE_ENV = 'production';
+
+  const start = async () => {
+    try {
+      const app = Fastify();
+      app.register(require('./app.js'));
+
+      await app.listen({ port: 3000, host: '0.0.0.0' });
+      console.log(`Worker ${process.pid} started and listening on ${app.server.address().port}`);
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  };
+
+  start();
 }
